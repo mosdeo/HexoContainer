18745285a0fb7581aaf7977c2281077d81d9b056
Lin, Kao-Yuan <mosdeo@gmail.com>
2021-07-25 22:23:22 +0800

    刪除不要的文件

--- run_hexo.sh
+++
@@ -1,6 +0,0 @@
-docker rm mosdeo_hexo -f
-docker run -i --rm --name mosdeo_hexo -p 4001:4000 -v /Users/lky/mosdeo_github_io/:/app/blog  mosdeo/hexo /bin/bash << EOF
-    cd /app/blog
-    hexo g
-    hexo d
-EOF
