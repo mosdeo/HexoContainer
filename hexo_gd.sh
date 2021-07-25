0b1c87c4f94a9bc77751941d0ada20fd4801eed4
Lin, Kao-Yuan <mosdeo@gmail.com>
2021-07-26 01:19:19 +0800

    修正容器名稱與映像名稱的的混淆不清

--- hexo_gd.sh
+++ hexo_gd.sh
@@ -1,12 +1,12 @@
-docker rm mosdeo_hexo -f
+docker rm hexo_gen_depoly -f
 docker run \
     -i --rm\
-    --name mosdeo_hexo -p 4001:4000 \
+    --name hexo_gen_depoly -p 4001:4000 \
     -v /Users/lky/mosdeo_github_io/:/app/blog \
     -v /Users/lky/code_repos/Stories/_posts:/app/blog/source/_posts \
     -v /Users/lky/code_repos/Stories/content:/app/blog/source/content \
     -v /Users/lky/.ssh:/root/.ssh \
-    mosdeo/hexo /bin/bash << EOF
+    mosdeo_hexo /bin/bash << EOF
     cd /app/blog
     hexo clean && hexo g
     git config --global user.email "mosdeo@gmail.com"
