c4dbb9da23c1fa727f7febb95f2480f3fc6bc04c
Lin, Kao-Yuan <mosdeo@gmail.com>
2021-07-25 21:08:29 +0800

    修正掛載路徑錯誤

--- hexo_gd.sh
+++ hexo_gd.sh
@@ -1,5 +1,5 @@
 docker rm mosdeo_hexo -f
-docker run -i --rm --name mosdeo_hexo -p 4001:4000 -v /Users/lky/mosdeo_github_io/:/blog  mosdeo/hexo /bin/bash << EOF
+docker run -i --rm --name mosdeo_hexo -p 4001:4000 -v /Users/lky/mosdeo_github_io/:/app/blog  mosdeo/hexo /bin/bash << EOF
     # hexo -v
     # rm -rf .deploy_git
     cd /app/blog
